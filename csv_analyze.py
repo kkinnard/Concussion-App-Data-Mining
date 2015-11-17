f895846aeffcc8269f8e685cd4a9595f156ec371
amunch <amunch@nd.edu>
2015-11-16 21:07:16 -0500

    Updates and added functions

---
+++ csv_analyze.py
@@ -0,0 +1,16 @@
+# Analyze the arrays.
+
+#!/usr/bin/python
+
+def average_array(list, num_rows, num_columns):
+	sum = [0] * num_columns
+	average = [0] * num_columns
+	# Sum up all the values for that particular category
+	for i in range(0, num_columns):
+		for j in range(0, num_rows):
+			sum[i] = sum[i] + list[j][i]
+	# Find the average for each of these categories
+  	for k in range(0, num_columns):
+		average[i] = sum[i] / num_columns
+
+
--- main.py
+++ main.py
@@ -4,13 +4,16 @@
 
 # Functions to edit the CSV files
 from csv_format import *
+from csv_analyze import *
 
 # Import the CSV Files 'Concussion.csv' and 'Control.csv' in the CSV folder.
 # Conc_list will be the list of the demographics for the concussed
 # Cont_list will be the list of the demographics for the control
 
 conc_rows = 94 # Start at 0.
+conc_columns = 11
 cont_rows = 485
+cont_columns = 11
 
 # Edit the input for day range and days symptoms lasted.
 # The rest of the data is 1's and 0's.
